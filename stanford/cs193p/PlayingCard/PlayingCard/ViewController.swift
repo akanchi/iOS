2e346a72c6fa417b27c6894b52de064fbe9602c7
akanchi <chenpx718@gmail.com>
2018-04-06 15:46:28 +0800

    drawing

---
+++ stanford/cs193p/PlayingCard/PlayingCard/ViewController.swift
@@ -0,0 +1,32 @@
+//
+//  ViewController.swift
+//  PlayingCard
+//
+//  Created by chen peixie on 4/6/18.
+//  Copyright © 2018 akanchi. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    var deck = PlayingCardDeck()
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+        for _ in 1...10 {
+            if let card = deck.draw() {
+                print("\(card)")
+            }
+        }
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
